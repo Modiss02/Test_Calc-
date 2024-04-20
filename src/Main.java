77b68c35977bb3dfc8e3ef796a232bf51b3cf22b
Modiss02 <rany022@icloud.com>
2024-04-20 16:42:25 +0500

    init commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите выражение: ");
+        String input = scanner.nextLine();
+        String result = Main.calc(input);
+        System.out.println(result);
+
+
+    }
+    public static String calc(String input) {
+        return null;
+    }
+}
